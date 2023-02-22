cc6099ba38c1ae261c1c5910d776033d6bbfcb54
evang-01 <124841899+evang-01@users.noreply.github.com>
2023-02-22 16:02:19 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import Tk
+from sys import argv
+from yoi import *
+
+
+def main():
+    win = Tk()
+    win.title('Yoi')
+    win.geometry('1560x720')
+    m = Yoi(win, path=(argv[1] if len(argv) > 1 else ''))
+    m.pack(fill='both', expand=True)
+    win.mainloop()
+
+
+if __name__ == '__main__':
+    main()
